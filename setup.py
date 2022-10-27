b057a31ace33a648ab76bb5d6c9b84f91e9655b8
lizgzil <lizgzil@hotmail.com>
2022-10-27 09:37:57 +0100

    correct setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""ojd_daps_skills."""
+"""abm_demo."""
 from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).parent
 
 
 setup(
-    name="ojd_daps_skills",
+    name="abm_demo",
     long_description=open(BASE_DIR / "README.md").read(),
     install_requires=read_lines(BASE_DIR / "requirements.txt"),
     packages=find_packages(exclude=["docs"]),
